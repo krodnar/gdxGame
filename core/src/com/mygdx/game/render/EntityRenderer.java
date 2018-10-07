9ac2d531312cf0b2861c4c9e06870154281ac220
krodnar <velkov.artem@gmail.com>
2018-10-07 21:24:55 +0300

    Remove old entity renderers

--- core/src/com/mygdx/game/render/EntityRenderer.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.game.render;
-
-public interface EntityRenderer {
-
-    void render(IGameRenderer renderer);
-
-    void dispose();
-}
--- core/src/com/mygdx/game/render/TiledEntityRenderer.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.game.render;
-
-import com.badlogic.gdx.math.Vector2;
-
-public interface TiledEntityRenderer extends EntityRenderer {
-
-    Vector2 getEntityBottom();
-}
